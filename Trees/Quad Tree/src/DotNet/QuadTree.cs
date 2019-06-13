a491b01d9af5dd546cbb3a1a34004b496aab3dcf
Ivan Saloponov <ivan.saloponov@acumatica.com>
2019-06-13 18:59:52 +0300

    Initialization root node.

--- Trees/Quad Tree/src/DotNet/QuadTree.cs
+++ Trees/Quad Tree/src/DotNet/QuadTree.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
 
 namespace QuadTree
 {
@@ -8,22 +11,22 @@ namespace QuadTree
 	{
 		private Node<T> Root { set; get; }
 
-		public QuadTree() => Root = new Node<T>();
-		public QuadTree(Vector3 max, Vector3 min) => Root = new Node<T>(max, min);
+		public QuadTree() => Root = null;
+		public QuadTree(Vector3 max, Vector3 min, Vector3 key, T value) => Root = new Node<T>(max, min, key, value);
 
 		public void Insert(Vector3 key, T value)
 		{
-			if (EqualityComparer<T>.Default.Equals(Root.value, default(T)))
+			//if (EqualityComparer<T>.Default.Equals(Root.value, default(T)))
+			if (Root == null)
 			{
-				Root.value = value;
-				Root.key = key;
-			}
+				Root = new Node<T>(key, value);
+			}/*
 			else if (Root.childs.All(x => x == default(Node<T>)))
 			{
 				var currentNode = Root;
 				
 				
-			}
+			}*/
 		}
 
 		private Vector3 PointBelongsToSquare(Vector3 point, Node<T> node)
@@ -45,17 +48,54 @@ namespace QuadTree
 			max = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
 			min = new Vector3(float.MinValue, float.MinValue, float.MinValue);
 
-			childs = new Node<T>[4] { default(Node<T>), default(Node<T>), default(Node<T>), default(Node<T>) };
-			parent = default(Node<T>);
-			value =  default(T);
+			InitRootNode();
+			value = default(T);
 		}
 
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void InitRootNode()
+		{
+			InitNullChilds();
+			parent = default(Node<T>);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void InitNewNode(Node<T> parent)
+		{
+			InitNullChilds();
+			this.parent = parent;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private Node<T>[] InitNullChilds() => childs = new Node<T>[4] { default(Node<T>), default(Node<T>), default(Node<T>), default(Node<T>) };
+		
 		public Node(Vector3 max, Vector3 min)
 		{
 			this.max = max; this.min = min;
+
+			InitRootNode();
 		}
 
-		
+		public Node(Vector3 max, Vector3 min, Vector3 key, T value)
+		{
+			this.max = max; this.min = min;
+			this.key = key;
+			this.value = value;
+
+			InitRootNode();
+		}
+
+		public Node(Vector3 key, T value)
+		{
+			this.max = key + Vector3.One * 100;
+			this.min = key - Vector3.One * 100;
+			this.key = key;
+			this.value = value;
+
+			InitRootNode();
+		}
+
+
 		private Vector3 max { get; set; }
 		private Vector3 min { get; set; }
 
