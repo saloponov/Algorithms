8975ec9a9714316e3b4d8acfa6aa7574144025c8
Ivan Saloponov <ivan.saloponov@acumatica.com>
2019-06-13 20:12:03 +0300

    Start search methods

--- Trees/Quad Tree/src/DotNet/QuadTree.cs
+++ Trees/Quad Tree/src/DotNet/QuadTree.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Numerics;
 using System.Threading;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
 
 
 namespace QuadTree
@@ -20,14 +24,23 @@ namespace QuadTree
 			if (Root == null)
 			{
 				Root = new Node<T>(key, value);
-			}/*
-			else if (Root.childs.All(x => x == default(Node<T>)))
+			}
+
+			var node = Root;
+			/*while (!node.childs.All(x => x == default(Node<T>)))
+			{
+				if ( key.X < )
+			}
+
+			else if (Root.childs.All(x => x == default(Node<T>))) //it is leaf
 			{
 				var currentNode = Root;
 				
 				
 			}*/
 		}
+
+		
 
 		private Vector3 PointBelongsToSquare(Vector3 point, Node<T> node)
 		{
@@ -43,32 +56,34 @@ namespace QuadTree
 
 	internal class Node<T>
 	{
-		public Node()
+		#region Fields
+		private Vector3 max { get; set; }
+		private Vector3 min { get; set; }
+
+		public Vector3 middle => (max + min) / 2;
+
+		public Vector3 key { get; set; }
+
+		public T value
+		{
+			get;
+			set;
+		}
+
+		public Node<T>[] childs;
+		public Node<T> parent;
+		#endregion
+
+		#region Constructors
+		/*public Node()
 		{
 			max = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
 			min = new Vector3(float.MinValue, float.MinValue, float.MinValue);
 
 			InitRootNode();
 			value = default(T);
-		}
+		}*/
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private void InitRootNode()
-		{
-			InitNullChilds();
-			parent = default(Node<T>);
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private void InitNewNode(Node<T> parent)
-		{
-			InitNullChilds();
-			this.parent = parent;
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private Node<T>[] InitNullChilds() => childs = new Node<T>[4] { default(Node<T>), default(Node<T>), default(Node<T>), default(Node<T>) };
-		
 		public Node(Vector3 max, Vector3 min)
 		{
 			this.max = max; this.min = min;
@@ -94,22 +109,133 @@ namespace QuadTree
 
 			InitRootNode();
 		}
+		#endregion
 
-
-		private Vector3 max { get; set; }
-		private Vector3 min { get; set; }
-
-		private Vector3 middle => (max + min) / 2;
-
-		public Vector3 key { get; set; }
-
-		public T value 
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void InitRootNode()
 		{
-			get;
-			set;
+			InitNullChilds();
+			parent = default(Node<T>);
 		}
 
-		public Node<T>[] childs;
-		public Node<T> parent;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void InitNewNode(Node<T> parent)
+		{
+			InitNullChilds();
+			this.parent = parent;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private Node<T>[] InitNullChilds() => childs = new Node<T>[8]
+		{
+			default(Node<T>), default(Node<T>), default(Node<T>), default(Node<T>),
+			default(Node<T>), default(Node<T>), default(Node<T>), default(Node<T>)
+		};
+		
+		public static Node<T> ChooseBranchNode(Node<T> current, Vector3 key)
+		{
+			if (current.middle.X <= key.X)
+			{
+				if (current.middle.Y <= key.Y)
+				{
+					if (current.middle.Z <= key.Z)
+						return current.childs[0];
+					else
+						return current.childs[1];
+				}
+				else
+				{
+					if (current.middle.Z <= key.Z)
+						return current.childs[2];
+					else
+						return current.childs[3];
+				}
+			}
+			else   // if (current.middle.X > key.X)
+			{
+				if (current.middle.Y <= key.Y)
+				{
+					if (current.middle.Z <= key.Z)
+						return current.childs[4];
+					else
+						return current.childs[5];
+				}
+				else
+				{
+					if (current.middle.Z <= key.Z)
+						return current.childs[6];
+					else
+						return current.childs[7];
+				}
+			}
+		}
+
+		private Node<T> ChooseBranchNode(Vector3 key)
+		{
+			if (middle.X <= key.X)
+			{
+				if (middle.Y <= key.Y)
+				{
+					if (middle.Z <= key.Z)
+						return childs[0];
+					else
+						return childs[1];
+				}
+				else
+				{
+					if (middle.Z <= key.Z)
+						return childs[2];
+					else
+						return childs[3];
+				}
+			}
+			else   // if (middle.X > key.X)
+			{
+				if (middle.Y <= key.Y)
+				{
+					if (middle.Z <= key.Z)
+						return childs[4];
+					else
+						return childs[5];
+				}
+				else
+				{
+					if (middle.Z <= key.Z)
+						return childs[6];
+					else
+						return childs[7];
+				}
+			}
+		}
+
+		public (Node<T>, Node<T>) FindNode(Vector3 key)
+		{
+			var current = this;
+			Node<T> parent = default(Node<T>);
+			
+			while (current != default(Node<T>))
+			{
+				if (current.key == key)
+					return (parent, current);
+
+				parent = current;
+				current = ChooseBranchNode(key);
+			}
+
+			return (parent, current);
+		}
+
+		public static Node<T> FindNode(Node<T> current, Vector3 key)
+		{
+			Node<T> parent = current;
+			while (current != default(Node<T>))
+			{
+				if (current.key == key)
+					return current;
+
+				current.ChooseBranchNode(key);
+			}
+			return current;
+		}
 	}
 }
